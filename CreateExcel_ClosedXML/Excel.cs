f8d3966b4b2867ffaee3192752557af1b714a673
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-19 12:26:17 -0500

    Program basic format ClosedXML

---
+++ CreateExcel_ClosedXML/Excel.cs
@@ -0,0 +1,15 @@
+﻿using ClosedXML.Excel;
+
+namespace CreateExcel_ClosedXML
+{
+    public class Excel
+    {
+        public static void CreateExcel(string path)
+        {
+            var workbook = new XLWorkbook();
+            var worksheet = workbook.Worksheets.Add("Sheet 1");
+            worksheet.Cell(1, 1).Value = "Hello World";
+            workbook.SaveAs(path);
+        }
+    }
+}
--- CreateExcel_ClosedXML/Program.cs
+++ CreateExcel_ClosedXML/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using CreateExcel_ClosedXML;
+
+Excel.CreateExcel(@"C:\ProjectsDEV\CreateExcel_ClosedXML\Excel\ReportClosedXML.xlsx");
