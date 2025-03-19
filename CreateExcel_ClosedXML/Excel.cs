0fff005a1c3e49ea941f467ba6c792a7f07565c4
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-19 14:53:12 -0500

    V2 manipulate cells

--- CreateExcel_ClosedXML/Excel.cs
+++ CreateExcel_ClosedXML/Excel.cs
@@ -1,4 +1,6 @@
 ﻿using ClosedXML.Excel;
+using CreateExcel_ClosedXML.Models;
+using System.Collections.Generic;
 
 namespace CreateExcel_ClosedXML
 {
@@ -10,6 +12,27 @@ namespace CreateExcel_ClosedXML
             var worksheet = workbook.Worksheets.Add("Sheet 1");
             worksheet.Cell(1, 1).Value = "Hello World";
             workbook.SaveAs(path);
+        }
+
+        public static void CreateExcel2(string path2)
+        {
+            var workbook2 = new XLWorkbook();
+            var worksheet2 = workbook2.Worksheets.Add("Sheet 2");
+            var customers = new List<Customer>
+            {
+                new Customer{Name="Juan",Age=18,Address="Calle33"},
+                new Customer{Name="Juan",Age=18,Address="Calle33"}
+            };
+
+            //Headers
+            worksheet2.Cell("A1").Value = "Name";
+            worksheet2.Cell("B1").Value = "Age";
+            worksheet2.Cell("C1").Value = "Address";
+
+            //Insert data directly from the list
+            worksheet2.Cell("A2").InsertData(customers);
+
+            workbook2.SaveAs(path2);
         }
     }
 }
--- CreateExcel_ClosedXML/Program.cs
+++ CreateExcel_ClosedXML/Program.cs
@@ -3,3 +3,4 @@
 using CreateExcel_ClosedXML;
 
 Excel.CreateExcel(@"C:\ProjectsDEV\CreateExcel_ClosedXML\Excel\ReportClosedXML.xlsx");
+Excel.CreateExcel2(@"C:\ProjectsDEV\CreateExcel_ClosedXML\Excel\ReportClosedXML2.xlsx");
