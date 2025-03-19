959a3d1b4b2d23a5c00fc30548c374823d1ccf49
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-19 12:33:44 -0500

    V2 add Customer model

---
+++ CreateExcel_ClosedXML/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace CreateExcel_ClosedXML.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; } = default!;
+        public int Age { get; set; }
+        public string Address { get; set; } = default!;
+    }
+}
