c182d793a3f9155493198cc12f4c16af4d45aa01
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-03-19 16:31:12 -0500

    Change model to Employee and add data

--- CreateExcel_ClosedXML/Excel.cs
+++ CreateExcel_ClosedXML/Excel.cs
@@ -42,10 +42,10 @@ namespace CreateExcel_ClosedXML
         //With format
         public static byte[] CreateExcel3(string path) 
         {
-            var _cusdata = GetCustomerData();
+            var _empdata = GetEmployeeData();
             using (XLWorkbook wb = new XLWorkbook())
             {
-                wb.AddWorksheet(_cusdata, "Customer Records");
+                wb.AddWorksheet(_empdata, "Employee Records");
                 using (MemoryStream ms = new MemoryStream())
                 {
                     wb.SaveAs(path);
@@ -55,32 +55,35 @@ namespace CreateExcel_ClosedXML
             }
             
         }
-        private static DataTable GetCustomerData() 
+        private static DataTable GetEmployeeData() 
         {
             DataTable dt = new DataTable();
-            dt.TableName = "CustomerData";
+            dt.TableName = "EmployeeeData";
             dt.Columns.Add("Name", typeof(string));
             dt.Columns.Add("Age", typeof(int));
             dt.Columns.Add("Address", typeof(string));
+            dt.Columns.Add("Email", typeof(string));
+            dt.Columns.Add("Salary", typeof(int));
+            dt.Columns.Add("Birthday", typeof(string));
 
-            var customers = new List<Customer>()
+            var employees = new List<Employee>()
             {
-                new Customer{Name="Andres",Age=18,Address="Carrera 22"},
-                new Customer{Name="Ana",Age=19,Address="Calle 45"},
-                new Customer{Name="Camilo",Age=25,Address="Carrera 33"},
-                new Customer{Name="Daniela",Age=34,Address="Avenida 22"},
-                new Customer{Name="Juan",Age=65,Address="Carrera 57"},
-                new Customer{Name="Jorge",Age=15,Address="Carrera 100"},
-                new Customer{Name="Gabriela",Age=87,Address="Carrera 44"},
-                new Customer{Name="Zaul",Age=56,Address="Avenida 93"},
-                new Customer{Name="Xavier",Age=46,Address="Calle 21"}
+                new Employee{Name="Andres",Age=18,Address="Carrera 22",Email="andres@gmail.com",Salary=1500000,Birthday="2020-12-12"},
+                new Employee{Name="Ana",Age=19,Address="Calle 45", Email = "andres@gmail.com", Salary = 1500000, Birthday = "2020-12-12"},
+                new Employee{Name="Camilo",Age=25,Address="Carrera 33", Email = "andres@gmail.com", Salary = 1500000, Birthday = "2020-12-12"},
+                new Employee{Name="Daniela",Age=34,Address="Avenida 22", Email = "andres@gmail.com", Salary = 1500000, Birthday = "2020-12-12"},
+                new Employee{Name="Juan",Age=65,Address="Carrera 57", Email = "andres@gmail.com", Salary = 1500000, Birthday = "2020-12-12"},
+                new Employee{Name="Jorge",Age=15,Address="Carrera 100", Email = "andres@gmail.com", Salary = 1500000, Birthday = "2020-12-12"},
+                new Employee{Name="Gabriela",Age=87,Address="Carrera 44", Email = "andres@gmail.com", Salary = 1500000, Birthday = "2020-12-12"},
+                new Employee{Name="Zaul",Age=56,Address="Avenida 93", Email = "andres@gmail.com", Salary = 1500000, Birthday = "2020-12-12"},
+                new Employee{Name="Xavier",Age=46,Address="Calle 21", Email = "andres@gmail.com", Salary = 1500000, Birthday = "2020-12-12"}
             };
 
-            if (customers.Count > 0) 
+            if (employees.Count > 0) 
             {
-                customers.ForEach(item => 
+                employees.ForEach(item => 
                 { 
-                    dt.Rows.Add(item.Name, item.Age, item.Address);
+                    dt.Rows.Add(item.Name, item.Age, item.Address, item.Email, item.Salary, item.Birthday);
                 });
             }
 
---
+++ CreateExcel_ClosedXML/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace CreateExcel_ClosedXML.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; } = default!;
+        public int Age { get; set; }
+        public string Address { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public int Salary { get; set; }
+        public string Birthday { get; set; } = default!;
+    }
+}
